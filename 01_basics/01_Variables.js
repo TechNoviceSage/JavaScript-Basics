43d44b7aac5b562e5cec0a209ea0156dbfabf656
Sumit Abhishek <121964929+TechNoviceSage@users.noreply.github.com>
2023-11-08 16:15:33 +0000

    datatypes done

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -2,3 +2,5 @@ const accountId = 144553
 let accountEmail="abhisumit50@gmail.com"
 var accountPassword="sumit@123"
 accountcity="Dehradun"
+
+console.table([accountId, accountEmail, accountPassword, accountcity])
---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,14 @@
+// alert() //we are using nodejs, not browser
+
+let name="Sumit" //String Data Type
+let age= 26 //Number Data Type
+
+//String Data Type => ""
+//Number Data Type => 2 to the power 53
+//boolean Data Type => true/flase
+//null => standalone value (represent and empty value)
+//undefined => no value assigned
+//symbol => used for uniquenes
+//object => 
+
+console.log(typeof age);
