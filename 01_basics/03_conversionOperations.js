d12ae017150d118d48a15e4984d6420d1d136957
Sumit Abhishek <121964929+TechNoviceSage@users.noreply.github.com>
2023-11-09 15:32:17 +0000

    datatype summary done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,10 @@
 //"33" => 33
 //"33abc" => NaN(Not A Number)
-// true => 1, flase => 2 
+// true => 1, flase => 2
+
+// **********************************Operations******************************* //
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros =["shaktiman", "Naagraj", "Doga"]
+{
+    name:"Sumit"
+}
+
+
+/* 
+////////Return type of variables in JavaScript//////
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
