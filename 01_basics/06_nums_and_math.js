7dcdf6979a3f202608fec4b0b998bab49ce67574
Sumit Abhishek <121964929+TechNoviceSage@users.noreply.github.com>
2023-11-12 05:41:43 +0000

    math and num done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,24 @@
+// const score= 400;
+
+// const balance=new Number(600);
+// console.log(balance)
+// console.log(balance.toFixed(2))
+// console.log(balance.toString().length);
+
+// const hundred =100000000;
+// console.log(hundred.toLocaleString('en-US'));
+
+
+// ++++++++++++++++  MATHS  ++++++++++++++++++++++++++ //
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,6,7,9,1,2));
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+const min=20;
+const max=40;
+
+console.log(Math.floor(Math.random(max-min)*10)+min);
