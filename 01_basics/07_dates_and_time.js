3e6fcfc4e41fa2ed4eef224d0c331c45fabd1336
Sumit Abhishek <121964929+TechNoviceSage@users.noreply.github.com>
2023-11-13 04:44:34 +0000

    date and time done

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,14 @@
+// ++++++++++++++++++DATES+++++++++++++++++++++++++ //
+// let myDate= new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toLocaleString());
+
+let myTimeStamp=Date.now();
+
+// console.log(myTimeStamp); // in miliseconds from 1970
+// console.log(Math.floor(Date.now()/1000)); /// Convert to seconds
+
+
