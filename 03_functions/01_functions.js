fb88fbcb8490baaa21798595eb5603e6af7e3f0f
Sumit Abhishek <121964929+TechNoviceSage@users.noreply.github.com>
2023-11-16 06:44:53 +0000

    function start

---
+++ 03_functions/01_functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+console.log("s");
+console.log("u");
+console.log("m");
+console.log("i");
+console.log("t");
+}
+// sayMyName() 
+
+function addTwoNumber(number1,number2){
+console.log(number1+number2);
+}
+addTwoNumber(3,4);
